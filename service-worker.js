a503fc5b28cc6c51ba276b76dc7166e810ec76cd
pezzaliapp <pezzalialessandro@gmail.com>
2025-01-24 20:59:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
-const CACHE_NAME = "locazione-cache-v1";
+const CACHE_NAME = "noleggio-cache-v1";
 const urlsToCache = [
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json",
-    "./icons/icons_crm-192x192.png",
-    "./icons/icons_crm-512x512.png"
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
